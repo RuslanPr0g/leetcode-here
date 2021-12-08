62b9ac1f26b6bd6e0aa825499773a0cca655d049
RuslanPr0g <theruslan.prog@gmail.com>
2021-12-08 15:23:14 +0200

    Improved binary serach by using that fact that local function can have access to parent function's parameters

--- LocalFuncitonsCiSharp/Ext.cs
+++ LocalFuncitonsCiSharp/Ext.cs
@@ -15,13 +15,35 @@
             if (compare == 0) { return middleIndex; }
 
             return compare > 0
-
                 ? BinarySearch(localSource, localValue, localComparer, startIndex, middleIndex - 1)
-
                 : BinarySearch(localSource, localValue, localComparer, middleIndex + 1, endIndex);
 
         }
 
         return BinarySearch(source, value, comparer ?? Comparer<T>.Default, 0, source.Count - 1);
+    }
+
+    internal static int BinarySearchWithClosureImproved<T>(this IList<T> source, T value,
+        IComparer<T>? comparer = null)
+    {
+        int BinarySearch(int startIndex, int endIndex)
+        {
+            if (startIndex > endIndex) { return -1; }
+
+            int middleIndex = startIndex + (endIndex - startIndex) / 2;
+
+            int compare = comparer.Compare(source[middleIndex], value);
+
+            if (compare == 0) { return middleIndex; }
+
+            return compare > 0
+                ? BinarySearch(startIndex, middleIndex - 1)
+                : BinarySearch(middleIndex + 1, endIndex);
+
+        }
+
+        comparer ??= Comparer<T>.Default;
+
+        return BinarySearch(0, source.Count - 1);
     }
 }
